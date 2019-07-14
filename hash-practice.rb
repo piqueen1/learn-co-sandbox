87e334f71137736763759ea615800cb53d624491
Finn Thye <piqueen@gmail.com>
2019-07-14 20:36:07 +0000

    Create school hash

--- hash-practice.rb
+++ hash-practice.rb
@@ -15,7 +15,6 @@ puts new_hash[key]
 new_hash[:new_key] = "I'd ask Lissa about it but I don't want to be a drag"
 
 puts new_hash[:new_key]
-=end
 
 shipping_manifest = {
   "whale bone corset" => 5,
@@ -37,4 +36,25 @@ end
 puts shipping_manifest["oil painting"]
 
 puts "There are now #{shipping_manifest["top hat"]} hats."
+=end
+
+instructor_list = ["Ian", "Kelson", "Finn"]
+
+student_list = ["Andrew", "Howard", "Amelie", "Hazel", "Parker", "Brian"]
+
+school = {
+  instructors: instructor_list,
+  students: student_list
+}
+
+puts school
+
+school[:classes] = ["Software Engineering", "Data Science"]
+
+puts school
+
+class_list = school[:classes]
+
+puts class_list.first 
+puts school[:classes].first 
 

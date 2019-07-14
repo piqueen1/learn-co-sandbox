85bed9c4e2c665ef14534983670699ad18a0bc42
Finn Thye <piqueen@gmail.com>
2019-07-14 19:28:41 +0000

    Checking for and adding keys

--- hash-practice.rb
+++ hash-practice.rb
@@ -1,3 +1,4 @@
+=begin
 new_hash = {
   :created => Time.now,
   :message => "Hello world!"
@@ -6,3 +7,34 @@ new_hash = {
 key = :message
 
 puts new_hash[key]
+
+new_hash[key] = "My head feels weird"
+
+puts new_hash[key]
+
+new_hash[:new_key] = "I'd ask Lissa about it but I don't want to be a drag"
+
+puts new_hash[:new_key]
+=end
+
+shipping_manifest = {
+  "whale bone corset" => 5,
+  "porcelain vase" => 2,
+  "oil painting" => 3,
+  "silverware" => 34,
+  "looom" => 1
+}
+
+shipping_manifest["oil painting"] = shipping_manifest["oil painting"] + 1
+
+if shipping_manifest["top hat"]
+  shipping_manifest["top hat"] += 1
+else
+  puts "Key being created now!"
+  shipping_manifest["top hat"] = 1
+end
+
+puts shipping_manifest["oil painting"]
+
+puts "There are now #{shipping_manifest["top hat"]} hats."
+
